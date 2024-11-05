8b69bdb3c73c8de7221536a9f24b1bb57344d423
jayesh-pixel <jayeshpandey11789@gmail.com>
2024-11-05 19:47:35 +0530

    delete

--- Downloads/New folder (1)/New folder/database/index.js
+++
@@ -1,3 +0,0 @@
-import { UpdateUser,Updatewinnigs,UpdateDashBoard,UpdateMegacontest } from "./update";
-import { ReadUser,ReadDashBoard } from "./read";
-export {UpdateUser,ReadUser,Updatewinnigs,ReadDashBoard,UpdateDashBoard,UpdateMegacontest};
